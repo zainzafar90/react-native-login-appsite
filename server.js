d38a68357f049f65c9ec206b42877423d69a99c1
Anton Krasovsky <anton.krasovsky@gmail.com>
2016-11-02 22:33:47 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const aasa = {
+  applinks: {
+    apps: [],
+    details: [
+      {
+        appID: process.env.APP_ID,
+        paths: [ "*" ]
+      }
+    ]
+  }
+};
+
+const PORT = 8080;
+
+const app = express();
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.get('/.well-known/apple-app-site-association', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(aasa));
+});
+
+app.get('/app.html', function (req, res) {
+  res.render('app');
+});
+
+app.listen(PORT, function () {
+  console.log('Running on http://localhost:' + PORT);
+});
